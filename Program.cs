f5f1b14bcaadd6991a7a0b8dd26d5d766bbe1b83
TIM27 <sprinchak27@mail.ru>
2022-06-25 00:09:52 +0300

    пример задачи на квадрат

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HelloWord
+{
+class Program
+  { 
+       // Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя)  
+
+       static void Main(string[] args)
+      {
+          Console.WriteLine("Введите целое число: ");
+          // 1. Получаем число 
+          int numbers = Convert.ToInt32(Console.ReadLine());
+          // 2. Умножаем и записываем в отдельную переменную
+          int square = numbers * numbers;
+          // Или 3. Получаем квадрат в переменной  number
+          numbers*= numbers;
+
+          // 4. Вывод результата 
+          Console.WriteLine($"Квадрат числа: {square}");
+
+      }
+
+  }
+
+}
+
